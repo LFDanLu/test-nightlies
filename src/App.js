0df783040a88d95ea928e3d79d4d722d030bee76
Daniel Lu <dl1644@gmail.com>
2022-03-29 15:42:45 -0700

    failing tests but working build when using nightly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {Cell, Column, Row, TableBody, TableHeader, TableView} from '@react-spectrum/table';
+import {Provider} from '@react-spectrum/provider'
+import {theme} from '@react-spectrum/theme-default';
 
 function App() {
   return (
@@ -18,6 +21,27 @@ function App() {
           Learn React
         </a>
       </header>
+      <Provider theme={theme}>
+      <TableView aria-label="TableView with resizable columns" width={800} height={200}>
+        <TableHeader>
+          <Column allowsResizing defaultWidth={200} minWidth={175} maxWidth={300}>File Name</Column>
+          <Column allowsResizing defaultWidth="1fr" minWidth={175} maxWidth={500}>Size</Column>
+          <Column allowsResizing defaultWidth={200} minWidth={175} maxWidth={300}>Type</Column>
+        </TableHeader>
+        <TableBody>
+          <Row>
+            <Cell>2018 Proposal</Cell>
+            <Cell>PDF</Cell>
+            <Cell>214 KB</Cell>
+          </Row>
+          <Row>
+            <Cell>Budget</Cell>
+            <Cell>XLS</Cell>
+            <Cell>120 KB</Cell>
+          </Row>
+        </TableBody>
+      </TableView>
+      </Provider>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {Cell, Column, Row, TableBody, TableHeader, TableView} from '@react-spectrum/table';
 
 test('renders learn react link', () => {
+  expect(true).toBeTruthy()
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
